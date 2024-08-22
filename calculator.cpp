3d0f122a9e0346a80bb1866ceaa256c6fc76e633
spidermunking <105009002+spidermunking@users.noreply.github.com>
2024-08-22 10:56:45 -0600

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(void){
+    int base = 0;
+    int num2 = 0;
+    string tf;
+
+    cout << "Please enter a number \n";
+    cin >> base;
+    //base number that will change with different calculations
+    cout << "Do you want to add?(t or f)\n";
+    cin >> tf;
+    if (tf == t){ 
+      cout << "Pleace enter a number to add\n";
+      cin >> num2; 
+      base = base + num
+    } 
+    //seeing if they want a number to add and has them choose what it is
+    cout << "Do you want to subtract?(tor f)\n";
+    cin >> tf;
+    if (tf == t){
+      cout << "please enter a number to subtract\n";
+      cin >> num2
+    }
+    
+    return 0;
+}
