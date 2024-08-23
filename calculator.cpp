d6421c37fc730228999024e09276dd234c95d13b
spidermunking <105009002+spidermunking@users.noreply.github.com>
2024-08-23 10:51:01 -0600

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(void){
     int base = 0;
-    int num2 = 0;
+    int num = 0;
     string tf;
 
     cout << "Please enter a number \n";
@@ -17,6 +17,7 @@ int main(void){
       cout << "Pleace enter a number to add\n";
       cin >> num2; 
       base = base + num
+      cout << base\n;
     } 
     //seeing if they want a number to add and has them choose what it is
     cout << "Do you want to subtract?(tor f)\n";
@@ -24,7 +25,11 @@ int main(void){
     if (tf == t){
       cout << "please enter a number to subtract\n";
       cin >> num2
+      base = base - num2
+      cout << base\n;
     }
+    //seeing if they want to subtract and if they do it has them pick a number then calculates it.
+    cout << "Do you want to 
     
     return 0;
 }
